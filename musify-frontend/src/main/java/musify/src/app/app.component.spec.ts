20c666014ca8e37123d5aadcb319cc8270eca129
DanielStoev <dannybob63@gmail.com>
2024-05-22 23:28:53 +0300

    Fixed frontend module....

--- musify-frontend/src/main/java/musify/src/app/app.component.spec.ts
+++ musify-frontend/src/main/java/musify/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'musify-frontend' title`, () => {
+  it(`should have the 'musify' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('musify-frontend');
+    expect(app.title).toEqual('musify');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, musify-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, musify');
   });
 });
